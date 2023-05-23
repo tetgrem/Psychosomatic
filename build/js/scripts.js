08c189036c4b0d73fd9ac3e90e5a38a532efeda3
tetgrem <tetgrem@gmail.com>
2023-05-23 21:46:12 +0300

    package and questions sections ready

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -15,22 +15,83 @@
  //# sourceMappingURL=swiper-bundle.min.js.map
 'use strict'
 
-const targetBlock = document.querySelectorAll('.targets__item')
+function  targetBlock() {
+    const targetItem = document.querySelectorAll('.targets__item')
 
-targetBlock.forEach((item) => {
-    item.addEventListener('click', () => {
-        if (window.innerWidth <= 768 && item.classList.contains('active')) {
-            item.classList.remove('active')
-        } else {
-            document
-                .querySelectorAll('.targets__item')
-                .forEach(item => item.classList.remove('active'))
-            item.classList.add('active')
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 768) {
+            targetItem[0].classList.add('active')
         }
     })
-})
+
+    targetItem.forEach((item) => {
+
+        item.addEventListener('click', () => {
+            if (window.innerWidth <= 768 && item.classList.contains('active')) {
+                item.classList.remove('active')
+            } else {
+                document
+                    .querySelectorAll('.targets__item')
+                    .forEach(item => item.classList.remove('active'))
+                item.classList.add('active')
+            }
+        })
+    })
+}
+targetBlock()
+
+
+function packageBlock() {
+    const packageItem = document.querySelectorAll('.package__item')
+
+    packageItem.forEach((item) => {
+        if (window.innerWidth > 992) {
+            item.classList.add('active')
+        }
+
+        window.addEventListener('resize', () => {
+            if (window.innerWidth > 992) {
+                item.classList.add('active')
+            }
+            if (window.innerWidth <= 992) {
+                item.classList.remove('active')
+            }
+        })
+
+        item.addEventListener('click', () => {
+            if (item.classList.contains('active') && window.innerWidth <= 992) {
+                item.classList.remove('active')
+            } else if (window.innerWidth <= 992) {
+                document
+                    .querySelectorAll('.package__item')
+                    .forEach(item => item.classList.remove('active'))
+                item.classList.add('active')
+            }
+        })
+    })
+}
+packageBlock()
+
+
+function packageHideBlock() {
+    const packageBtn = document.querySelector('.package__btn')
+    const packageHide = document.querySelector('.package__hide-block')
+
+    packageBtn.addEventListener('click', () => {
+        if (packageHide.classList.contains('active')) {
+            packageHide.classList.remove('active')
+            packageBtn.classList.remove('active')
+        } else {
+            packageHide.classList.add('active')
+            packageBtn.classList.add('active')
+        }
+    })
+}
+packageHideBlock()
+
 
 function burgerMenu() {
+    const menuItem = document.querySelectorAll('.menu__item')
     const burger = document.querySelector('.burger')
     const menu = document.querySelector('.menu')
     const body = document.querySelector('body')
@@ -44,6 +105,14 @@ function burgerMenu() {
             burger.classList.remove('active')
             body.classList.remove('locked')
         }
+    })
+
+    menuItem.forEach((item) => {
+        item.addEventListener('click', () => {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        })
     })
     // Вот тут мы ставим брейкпоинт навбара
     window.addEventListener('resize', () => {
@@ -85,5 +154,13 @@ const swiper = new Swiper('.swiper', {
       },
     }
 });
+
+
+const maskElement = document.querySelector('.phone__input')
+const maskOptions = {
+    mask: '+{38}(000)000-00-00',
+}
+const mask = IMask(maskElement, maskOptions)
+
 
 

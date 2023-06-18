b5fa6b4f9fd360b0c09f3d1a745868593d9da0f4
tetgrem <tetgrem@gmail.com>
2023-06-18 13:56:47 +0300

    last commit

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -163,4 +163,52 @@ const maskOptions = {
 const mask = IMask(maskElement, maskOptions)
 
 
+// Аккордеон
+function accordion() {
+    const items = document.querySelectorAll('.accordion__item-trigger')
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            const parent = item.parentNode
+            if (parent.classList.contains('accordion__item-active')) {
+                parent.classList.remove('accordion__item-active')
+            } else {
+                document
+                    .querySelectorAll('.accordion__item')
+                    .forEach(child => child.classList.remove('accordion__item-active'))
+                parent.classList.add('accordion__item-active')
+            }
+        })
+    })
+}
+accordion()
+
+// Модальное окно
+function bindModal(trigger, modal, close) {
+    trigger = document.querySelector(trigger),
+        modal = document.querySelector(modal),
+        close = document.querySelector(close)
+
+    const body = document.body
+
+    trigger.addEventListener('click', e => {
+        e.preventDefault()
+        modal.style.display = 'flex'
+        body.classList.add('locked')
+    });
+    close.addEventListener('click', () => {
+        modal.style.display = 'none'
+        body.classList.remove('locked')
+    });
+    modal.addEventListener('click', e => {
+        if (e.target === modal) {
+            modal.style.display = 'none'
+            body.classList.remove('locked')
+        }
+    })
+}
+
+// ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
+// ВТОРОЙ аргумент - класс самого модального окна.
+// ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
+bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
 
